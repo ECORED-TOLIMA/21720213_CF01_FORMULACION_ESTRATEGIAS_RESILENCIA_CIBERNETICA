4c3eda06c1e221a23e22ac56a1833b4ab2bd5a63
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2025-11-04 19:45:43 -0500

    Update hallazgos & CFA

--- src/config/global.js
+++ src/config/global.js
@@ -213,7 +213,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CFA_XX_DU.pdf',
+        download: 'downloads/CFA_01_2720213_DU.pdf',
       },
       {
         icono: 'fas fa-download',
